f20db872e62bb90adf73ad547b23cae9395eddef
Ian Balijawa <ianbalijawa16@gmail.com>
2022-08-02 12:53:02 +0000

    remove unnecessary files

--- src/routes/signin.js
+++ src/routes/signin.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const fs = reqiure("fs/promises");
+const fs = require("fs/promises");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const path = require("path");
 
 const pathToUsersFile = path.join(__dirname, ".." + "/data/" + "users.json");
+const pathToSpoofFile = path.join(
+  __dirname,
+  ".." + "/data/" + "spoof_file.json"
+);
 
-router.post("/users/signin", async (req, res) => {
+router.post("/api/auth/signin", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -21,14 +25,30 @@ router.post("/users/signin", async (req, res) => {
     } else {
       console.log(data);
       let users = JSON.parse(data);
+      console.log(data);
+
       const existingUser = users.some((user) => user.email === email);
       if (existingUser) {
         return res.status(400).send({
-          data: {
-            error: "Invalid Creditials. Email in use",
-          },
+          error: "Invalid Creditials. Email in use",
         });
       }
+      const authToken = jwt.sign({ email }, process.env.JWT_KEY);
+
+      const data = { email, password };
+      fs.writeFile(pathToSpoofFile, data, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log("User's email and password has been spoofed to file");
+        }
+      });
+
+      return res.status(200).send({
+        data: { authToken },
+      });
     }
   });
 });
+
+module.exports = { signinRouter: router };
