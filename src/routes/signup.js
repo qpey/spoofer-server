57688503895b0e729a05bd9504e3d53bc7a0c542
leonidasdassis0047 <leonidasdassis@gmail.com>
2022-08-02 17:56:28 +0300

    [feature:signup]signup feature done.

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,17 +1,30 @@
-const router = require("express");
-const fs = require("fs");
+const router = require("express").Router();
+const fs = require("fs/promises");
+const path = require("path");
 
-router.post("/users/signup", async function (req, res, next) {
+router.post("/api/auth/signup", async function (req, res, next) {
   try {
     const { first_name, last_name, email, password } = req.body;
-    let users = fs.readFileSync(`${__dirname}/data/users.json`, "utf-8");
+    const FILE = path.resolve(`${__dirname}`, "..", "data", "users.json");
+    let users = await fs.readFile(FILE, { encoding: "utf-8" });
     users = JSON.parse(users);
-    console.log(users);
 
-    return res.status(200).json({ error: false, data: users });
+    if (users.some((user) => user.email == email)) {
+      return res
+        .status(200)
+        .json({ error: true, message: "Invalid user credentials" });
+    }
+
+    const id = users.length + 1;
+    const newUser = { id, first_name, last_name, email, password };
+
+    users.push(newUser);
+    await fs.writeFile(FILE, JSON.stringify(users), { encoding: "utf-8" });
+
+    res.status(200).json({ error: false, data: users });
   } catch (error) {
     res.status(500, error.message);
   }
 });
 
-module.exports = router;
+module.exports = { signupRouter: router };
