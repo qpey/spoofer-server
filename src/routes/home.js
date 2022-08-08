4182ea3e6fef9d469596137cea508280eed2df79
Ian Balijawa <ianbalijawa16@gmail.com>
2022-08-08 09:42:18 +0000

    change home route messafe

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.send("Welcome to spoofy");
+  res.send("Welcome to Spoofy API");
 });
 
 module.exports = { homeRouter: router };
